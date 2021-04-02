f3df54a4f140d958604c56da171da5ef1715548e
JigSaW <misho_1998@mail.ru>
2021-04-02 15:38:36 +0300

    news

--- Collection_of_programming_problems/11_3_medium_29.py
+++ Collection_of_programming_problems/11_3_medium_29.py
@@ -1,6 +1,11 @@
-a = bool(input('Введите A = '))
-b = bool(input('Введите B = '))
-c = bool(input('Введите C = '))
-print(a, b ,c)
-res = not (a & (not b)) or (a or (not c))
-print(f'{a}, {b}, {c} --> {res}')
+def expr(a, b, c):
+	res = not (a & (not b)) or (a or (not c))
+	return f'{a}, {b}, {c} --> {res}'
+print(expr(True, True, True))
+print(expr(True, True, False))
+print(expr(True, False, True))
+print(expr(True, False, False))
+print(expr(False, False, False))
+print(expr(False, True, True))
+print(expr(False, False, True))
+print(expr(False, True, False))
