77fa73eb8ae353dd0ca970b71696dc6537435f55
JigSaW <misho_1998@mail.ru>
2021-04-03 19:54:42 +0300

    all

---
+++ Collection_of_programming_problems/26_7_medium_28.py
@@ -0,0 +1,3 @@
+for i in range(121, 921, 200):
+	for j in range(i, i + 10):
+		print(j * j * j)
---
+++ Collection_of_programming_problems/30_8_medium_27.py
@@ -0,0 +1,34 @@
+from math import log, cos, pi
+import pylab
+import numpy as np
+
+def f(x):
+	if x > 2.5:
+		return x
+	elif 0 <= x and x <= 2.5:
+		return 1 + x**2
+	else:
+		return x**5 - log(abs(cos(x)))
+
+xmin = -pi / 2
+xmax = 2*pi
+dx = 0.2
+xlist = np.arange(xmin, xmax, dx)
+ylist = [f(x) for x in xlist]
+print(xlist)
+print(ylist)
+pylab.plot(xlist, ylist)
+pylab.show()
+# y = np.vectorize(f)
+# # y1 = lambda x: 1 + x**2
+# # y2 = lambda x: x**5 - log(abs(cos(x)))
+# fig = plt.subplots()
+# x = np.linspace(-pi / 2, 2*pi, 2)
+# plt.plot(x, y(x))
+# # plt.plot(x, y1(x))
+# # plt.plot(x, y2(x))
+# # показываем график
+# plt.show()
+
+
+
