e3fcb3d3ab6c3d8c9f980b881170bf145ee9a350
JigSaW <misho_1998@mail.ru>
2021-05-12 17:21:57 +0300

    c++

---
+++ transform_if/transform_if/transform_if.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+template <typename InIter, typename OutIter, typename Predicate, typename Action>
+OutIter TransformIf(InIter first, InIter last, OutIter out, Predicate condition, Action f) {
+    while (first != last) {
+        if (condition(*first))
+            *out++ = f(*first);
+        ++first;
+    }
+}
